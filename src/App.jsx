debcd3f71b7691e74bfcc7443c693b23ee1569cb
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2023-12-11 22:53:05 +0600

    set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quidem
+      illo laboriosam iste delectus sapiente blanditiis, ad suscipit vitae
+      veritatis repudiandae quibusdam dolore impedit. Incidunt reprehenderit
+      quasi quas modi saepe.
+    </div>
+  );
+}
+
+export default App;
